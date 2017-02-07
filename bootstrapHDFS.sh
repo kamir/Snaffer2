954433fc371f07f0287eed6cb4dfe4aa164fc497
Mirko <mirko.kaempf@gmail.com>
2017-02-07 14:25:26 +0100

    cleaned some stuff

--- bootstrapHDFS.sh
+++
@@ -1,7 +0,0 @@
-
-hadoop fs -mkdir /user/cloudera
-hadoop fs -mkdir /user/cloudera/TCPDUMP/
-hadoop fs -mkdir /user/cloudera/TCPDUMP/raw
-hadoop fs -mkdir /user/cloudera/TCPDUMP/META
-hadoop fs -put ./schema/* /user/cloudera/TCPDUMP/META
-
--- both.sh
+++
@@ -1,3 +0,0 @@
-sudo ./snaff.sh lo 100000 &
-sudo ./snaff.sh eth0 100000 &
-
